d2fe89f2ac44db56baebb6b78f988b2d849a2335
Dominique-HL <dominiquehenriquezleiva@gmail.com>
2021-06-14 15:33:30 -0400

    first commit

--- Bright_ideas/settings.py
+++ Bright_ideas/settings.py
@@ -124,10 +124,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    # os.path.join(BASE_DIR, 'static_files/css'),
-    # "static_files/css",
-    ("all_styles", "apps/static/css"),
-    ("all_images", "apps/static/images"),
-    ("all_js", "apps/static/scripts"),
-]
